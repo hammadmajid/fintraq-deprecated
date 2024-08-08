548b9130602d443ad36b6566ad703fa67aea180f
Hammad Majid <hammadmajid@proton.me>
2024-08-09 00:13:48 +0500

    Get the list of all records from from each account of the user

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -14,3 +14,22 @@ export interface AccountData {
 export interface AccountList {
 	accounts: AccountData[]
 }
+
+export interface RecordData {
+	id: string
+	accountId: string
+	amount: number
+	created: string
+	type: string
+	category: string
+}
+
+export interface AccountRecord {
+	records: RecordData[]
+	id: string
+	name: string
+}
+
+export interface AllRecords {
+	records: AccountRecord[]
+}
--- src/routes/app/records/+page.server.ts
+++ src/routes/app/records/+page.server.ts
@@ -1,10 +1,44 @@
-import { redirect } from '@sveltejs/kit'
+import { error as svelteError, redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
+import type { AllRecords, RecordData } from '$lib'
 
-export const load: PageServerLoad = async ({ locals: { safeGetSession } }) => {
+export const load: PageServerLoad = async ({ locals: { safeGetSession, supabase } }) => {
 	const { session } = await safeGetSession()
 
 	if (!session) {
 		redirect(303, '/auth/signin')
 	}
+
+	const { data, error } = await supabase
+		.from('accounts')
+		.select('id,name')
+		.eq('user', session.user.id)
+
+	if (!data || error) {
+		svelteError(500, 'Could not fetch accounts')
+	}
+
+	const allRecords: AllRecords = { records: [] }
+	for (const account of data) {
+		const { data: records, error } = await supabase
+			.from('records')
+			.select('id,account,amount,created_at,type,category')
+			.eq('account', account.id)
+
+		if (error) {
+			svelteError(500, 'Could not fetch records')
+		}
+
+		allRecords.records.push({
+			id: account.id,
+			name: account.name,
+			records: records.map((record: any) => ({
+				...record,
+				accountId: record.account,
+				created: record.created_at
+			})) as RecordData[]
+		})
+	}
+
+	return allRecords
 }
